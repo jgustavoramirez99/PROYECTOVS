562a8a9310ebd781820a2821682c25c914c96be4
Gustavo <jgustavo.ramirez99@gmail.com>
2026-04-04 18:49:13 -0500

    Se agrego un commit

--- Dev01/Form1.cs
+++ Dev01/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Dev01
 {
     public partial class Form1 : Form
@@ -5,6 +7,17 @@ namespace Dev01
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 10;
+            int b = 5;
+
+            
+            
+            int Suma = a + b;
+            textBox1.Text = Suma.ToString();
         }
     }
 }
